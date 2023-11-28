a23d9f10e8973a9b7a4748ffcdad7853b8dad518
J Wokaty <jwokaty@users.noreply.github.com>
2023-11-28 01:49:52 -0500

    Small fixes

--- rrbot_simulation_files/rrbot_python/rrbot_python/pd_controller_client.py
+++ rrbot_simulation_files/rrbot_python/rrbot_python/pd_controller_client.py
@@ -13,13 +13,17 @@ from std_msgs.msg import Float32, Float32MultiArray, Float64MultiArray, String
 
 
 class pd_controller_client(Node):
-    def __init__(self):
+
+    def __init__(self, data):
 
         super().__init__('pd_controller_client')
         self.sample_time = 0.1  #publish every 100ms
         self.timer = self.create_timer(self.sample_time,
                                        self.publish_effort_callback)
+        self.rate = self.create_rate(100)
 
+        self.q_ref = Float64MultiArray(data=data)
+        self.q_measured = Float64MultiArray(data=[0.0, 0.0, 0.0])
         self.publisher_ = self.create_publisher(
             Float64MultiArray, "/forward_position_controller/commands", 10)
         self.subscription = self.create_subscription(JointState,
@@ -30,8 +34,6 @@ class pd_controller_client(Node):
                                          'move_to_joint_positions')
         self.request = MoveToJointPositions.Request()
 
-        self.q_ref = Float64MultiArray()
-        self.q_ref = [0, 0, 0]
         self.joint_limit = 0.05
 
         # self.Kp = 1.0 #position gain
@@ -40,54 +42,62 @@ class pd_controller_client(Node):
     def send_request(self):
         self.request.q_ref = self.q_ref
         self.request.q_measured = self.q_measured
-        self.future = self.cli.call_async(self.request)
+        self.future = self.client.call_async(self.request)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
 
     def q_measured_callback(self, msg):
-        self.q_measured = Float64MultiArray()
-        self.q_measured = [0, 0, 0]
-        self.q_measured[0] = msg.position[0]
-        self.q_measured[1] = msg.position[1]
-        self.q_measured[2] = msg.position[2]
+        data = [msg.position[0], msg.position[1], msg.position[2]]
+        self.q_measured = Float64MultiArray(data=data)
+        return self.q_measured
 
     def publish_effort_callback(self):
-        while (not self.joints_within_limits()):
-            result = self.send_request()
-            self.publisher_.publish(result)
-            self.get_logger().info(
-                'REF:  q1: %f q2: %f q3: %f' %
-                (self.q_ref[0], self.q_ref[1], self.q_ref[2]))
-            self.get_logger().info(
-                'MEAS: q1: %f q2: %f q3: %f' %
-                (self.q_measured[0], self.q_measured[1], self.q_measured[2]))
-            self.get_logger().info('EFF:  q1: %f q2: %f q3: %f' %
-                                   (result[0], result[1], result[2]))
+        result = self.send_request()
+        self.publisher_.publish(result)
+        self.get_logger().info(
+            'REF:  q1: %f q2: %f q3: %f' %
+            (self.request.q_ref.data[0], self.request.q_ref.data[1],
+             self.request.q_ref.data[2]))
+        self.get_logger().info(
+            'MEAS: q1: %f q2: %f q3: %f' %
+            (self.q_measured.data[0], self.q_measured.data[1],
+             self.q_measured.data[2]))
+        self.get_logger().info('EFF:  q1: %f q2: %f q3: %f' %
+                               (result[0], result[1], result[2]))
 
     def joints_within_limits(self):
-        q1_within_limits = ((self.q_measured[0] <=
-                             (self.request.q_ref[0] + self.joint_limit))
-                            and (self.q_measured[0] >=
-                                 (self.request.q_ref[0] - self.joint_limit)))
-        q2_within_limits = ((self.q_measured[1] <=
-                             (self.request.q_ref[1] + self.joint_limit))
-                            and (self.q_measured[1] >=
-                                 (self.request.q_ref[1] - self.joint_limit)))
-        q3_within_limits = ((self.q_measured[2] <=
-                             (self.request.q_ref[2] + self.joint_limit))
-                            and (self.q_measured[2] >=
-                                 (self.request.q_ref[2] - self.joint_limit)))
+        q1_within_limits = ((self.q_measured.data[0] <=
+                             (self.request.q_ref.data[0] + self.joint_limit))
+                            and (self.q_measured.data[0] >=
+                                 (self.request.q_ref.data[0] - self.joint_limit)))
+        q2_within_limits = ((self.q_measured.data[1] <=
+                             (self.request.q_ref.data[1] + self.joint_limit))
+                            and (self.q_measured.data[1] >=
+                                 (self.request.q_ref.data[1] - self.joint_limit)))
+        q3_within_limits = ((self.q_measured.data[2] <=
+                             (self.request.q_ref.data[2] + self.joint_limit))
+                            and (self.q_measured.data[2] >=
+                                 (self.request.q_ref.data[2] - self.joint_limit)))
         return q1_within_limits and q2_within_limits and q3_within_limits
 
+
+def parse_data(data):
+    """Parse '{data: {a, b, c}}' for floats a, b, c"""
+
+    nums = data.split(":")[1].strip(" {}").split(",")
+    return [float(num.strip(" ")) for num in nums]
 
 def main(args=None):
     rclpy.init(args=args)
 
-    pd_client = pd_controller_client()
-    pd_client.q_ref = (sys.argv[1])
-    # response = pd_controller_client.send_request(int(sys.argv[1]))
+    pd_client = pd_controller_client(data=parse_data(sys.argv[1]))
+    #pd_client.q_ref = (sys.argv[1])
+    response = pd_client.send_request()
 
-    rclpy.spin(pd_client)
+    #rclpy.spin(pd_client)
+    while (not pd_client.joints_within_limits()):
+        rclpy.spin_once(pd_client)
+        pd_client.rate.sleep()
 
     pd_client.destroy_node()
     rclpy.shutdown()
--- rrbot_simulation_files/rrbot_python/rrbot_python/pd_controller_server.py
+++ rrbot_simulation_files/rrbot_python/rrbot_python/pd_controller_server.py
@@ -22,30 +22,33 @@ class pd_controller_service(Node):
         self.Kd = 1.0  #derivative gain
         # self.last_q_effort = [0.0, 0.0, 0.0]
 
-        self.last_time = 0.0
+        #self.last_time = 0.0
 
     def pd_controller_callback(self, request, response):
         # calculate each joint's error from measured to reference
-        q_error = [(request.q_ref[0] - request.q_measured[0]),
-                   (request.q_ref[1] - request.q_measured[1]),
-                   (request.q_ref[2] - request.q_measured[2])]
+        q_error = [(request.q_ref.data[0] - request.q_measured.data[0]),
+                   (request.q_ref.data[1] - request.q_measured.data[1]),
+                   (request.q_ref.data[2] - request.q_measured.data[2])]
 
+        # Not part of request; we could change the srv to include it or use the
+        # same value?
         # get the change in time since last sample
-        self.dt = request.curr_time - self.last_time
+        #self.dt = request.curr_time - self.last_time
+        self.dt = 0.01 
 
         # calculate each joint's effort (Kp*error + Kd*(error/dt))
         q1_effort = (self.Kp * q_error[0]) + (self.Kd * (q_error[0] / self.dt))
         q2_effort = (self.Kp * q_error[1]) + (self.Kd * (q_error[1] / self.dt))
         q3_effort = (self.Kp * q_error[2]) + (self.Kd * (q_error[2] / self.dt))
-        response = [q1_effort, q2_effort, q3_effort]
+        response = Float64MultiArray(data=[q1_effort, q2_effort, q3_effort])
 
         # re-set last-time for next function call
-        self.last_time = request.curr_time
+        #self.last_time = request.curr_time
         return response
 
 
-def main(args=None):
-    rclpy.init(args=args)
+def main():
+    rclpy.init()
 
     pd_service = pd_controller_service()
     rclpy.spin(pd_service)
